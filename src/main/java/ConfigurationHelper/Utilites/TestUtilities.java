b3d6f2e65f5a4ee8b89ad2c5ad7a68588a36f9fe
Sivaraman M <msivaraman1998@gmail.com>
2024-08-06 11:45:14 +0530

    test utli

--- src/main/java/ConfigurationHelper/Utilites/TestUtilities.java
+++ src/main/java/ConfigurationHelper/Utilites/TestUtilities.java
@@ -1,5 +1,15 @@
 package ConfigurationHelper.Utilites;
 
+import org.openqa.selenium.WebElement;
+
 public class TestUtilities {
 
+    public void uploadFile(WebElement locator){
+        try{
+        locator.sendKeys(System.getProperty("user.dir") +"/src/test/resources/TestData/UtilFiles/Test file.pdf");
+    }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
